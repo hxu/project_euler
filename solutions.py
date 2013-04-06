a0fa76ffe239dfaf7b3e20cdecca8c3ca05485b5
Han Xu <hgcrpd@gmail.com>
2013-04-06 17:44:39 +0800

    solution 9

--- solutions.py
+++ solutions.py
@@ -9,6 +9,11 @@ def sieve(limit):
     return sieve_of_eratosthenes(limit)
 
 
+def prod(iterable):
+    from operator import mul
+    return reduce(mul, iterable, 1)
+
+
 def sieve_of_eratosthenes(limit):
     # Uses Sieve of Eratosthenes to get a list of primes up to limit
     # Pretty slow for larger limits, seems like the union is the problem?
@@ -209,9 +214,6 @@ class Solution7(Solution):
 
 class Solution8(Solution):
     def solve(self):
-        def prod(iterable):
-            from operator import mul
-            return reduce(mul, iterable, 1)
 
         num = 7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450
         digits = [int(x) for x in str(num)]
@@ -222,4 +224,48 @@ class Solution8(Solution):
 
         self.answer = maximum
 
-Solution8().run()
+#Solution8().run()
+
+
+class Solution9(Solution):
+    #A Pythagorean triplet is a set of three natural numbers, a  b  c, for which,
+
+    #a2 + b2 = c2
+    #For example, 32 + 42 = 9 + 16 = 25 = 52.
+
+    #There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+    #Find the product abc.
+
+    # a < b < c < 999
+    # a + b + c = 1000
+    # a2 + b2 = c2
+    #
+    # c = sqrt(a2 + b2) = 1000 - a - b
+    # a + b = 1000 - c
+    # c must be a perfect square as well, since it is the square root of two
+    # integers
+    # a + b + sqrt(a2 + b2) = 1000
+    def is_perfect_square(self, num):
+        return math.sqrt(num).is_integer()
+
+    def pythagorean_triple(self, m, n):
+        a = (m ** 2) - (n ** 2)
+        b = 2 * m * n
+        c = (m ** 2) + (n ** 2)
+        return (a, b, c)
+
+    def solve(self):
+        self.answer = None
+        # find all perfect squares less than 999
+#        cs = [x for x in range(1, 1000) if self.is_perfect_square(x)]
+        m = 2
+        while self.answer is None:
+            for n in range(1, m):
+                triple = self.pythagorean_triple(m, n)
+                print(triple)
+                if sum(triple) == 1000:
+                    self.answer = prod(triple)
+            m += 1
+
+
+Solution9().run()
